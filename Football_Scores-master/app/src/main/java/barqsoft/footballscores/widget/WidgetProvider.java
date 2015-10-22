5bcbd19efb0b1c629eb934833b98be98281993c0
KevinTaron <kevin.taron@gmail.com>
2015-10-22 23:36:04 +0200

    football scores with base widget

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
@@ -0,0 +1,18 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+import barqsoft.footballscores.service.myFetchService;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        context.startService(new Intent(context, myFetchService.class));
+    }
+
+
+}
