93902f28516ab209e75540f77301501a372a655e
KevinTaron <kevin.taron@gmail.com>
2015-10-22 23:26:55 +0200

    working barcode scanner and offline problem fixed

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/Scanner/ScannerActivity.java
@@ -0,0 +1,50 @@
+package it.jaschke.alexandria.Scanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class ScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        List<BarcodeFormat> barcodeFormats = new ArrayList<BarcodeFormat>();
+        barcodeFormats.add(BarcodeFormat.EAN_13);
+        mScannerView.setFormats(barcodeFormats);
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("Barcode", result.getText());
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+}
